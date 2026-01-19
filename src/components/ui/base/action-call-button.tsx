d57be8fc02dcdec025d90ba1c4774b10d72ff51b
2hoch1 <217781969+2hoch1@users.noreply.github.com>
2026-01-19 07:36:47 +0100

    feat: landing page finish (#106)
    * feat: informationBlock component
    
    * feat: informationBlockArea component
    
    * feat: feat: actionCallButton component
    
    * chore: update landing page
    
    * chore: delete page

---
+++ src/components/ui/base/action-call-button.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+function ActionCallButton() {
+  return (
+    <Button id='action-call-button' className='sticky z-50'>
+      Save the World!
+    </Button>
+  );
+}
+
+export { ActionCallButton };
